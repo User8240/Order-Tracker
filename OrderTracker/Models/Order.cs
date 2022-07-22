f7f6675733dc719c7e9e765b18529408f7753764
Grace Kostanich <fl5812597@gmail.com>
2022-07-22 13:03:18 -0700

    Clear & get all instances in the list

--- OrderTracker/Models/Order.cs
+++ OrderTracker/Models/Order.cs
@@ -8,13 +8,23 @@ namespace OrderTracker.Models
     public string Description { get; set; }
     public int Id { get; }
     private static List<Order> _instances = new List<Order> {}; 
-     
+
     public Order(string orderName, string orderDescription)
     {
       Name = orderName;
       Description = orderDescription;
       _instances.Add(this);
       Id = _instances.Count;
+    }
+
+    public static List<Order> GetAll()
+    {
+      return _instances;
+    }
+    
+    public static void ClearAll()
+    {
+      _instances.Clear();
     }
 
       public static Order Find(int searchId)
