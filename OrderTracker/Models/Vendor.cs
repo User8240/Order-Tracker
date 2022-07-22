3bf4577d02da3e5e3a24f312653cd47621b31e87
Grace Kostanich <fl5812597@gmail.com>
2022-07-22 11:38:35 -0700

    Include ID, Vendor instances list, & Orders list in class and constructor | Add functionality for Id count and Find method to search by ID

--- OrderTracker/Models/Vendor.cs
+++ OrderTracker/Models/Vendor.cs
@@ -4,13 +4,22 @@ namespace OrderTracker.Models
 {
   public class Vendor
   {
+    private static List<Vendor> _instances = new List<Vendor> {};
     public string Name { get; set; }
-    public string Description { get; set; }
+    public int Id { get; }
+    public List<Order> Orders { get; set; }
 
-    public Vendor(string vendorName, string vendorDescription)
+    public Vendor(string VendorName)
     {
-      Name = vendorName;
-      Description = vendorDescription;
+      Name = VendorName;
+      _instances.Add(this);
+      Id = _instances.Count;
+      Orders = new List<Order>{};
+    }
+
+      public static Vendor Find(int searchId)
+    {
+      return _instances[searchId-1];
     }
 
   }
