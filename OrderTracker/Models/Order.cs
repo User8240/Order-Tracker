8e29d87f0485af332456febe0a24ef317df6a4e6
Grace Kostanich <fl5812597@gmail.com>
2022-07-22 11:43:56 -0700

    Include ID & Order instances list in class and constructor | Add functionality for Id count and Find method to search by ID

--- OrderTracker/Models/Order.cs
+++ OrderTracker/Models/Order.cs
@@ -6,12 +6,20 @@ namespace OrderTracker.Models
   {
     public string Name { get; set; }
     public string Description { get; set; }
-
+    public int Id { get; }
+    private static List<Order> _instances = new List<Order> {}; 
+     
     public Order(string orderName, string orderDescription)
     {
       Name = orderName;
       Description = orderDescription;
+      _instances.Add(this);
+      Id = _instances.Count;
     }
-      
+
+      public static Order Find(int searchId)
+    {
+      return _instances[searchId-1];
+    }  
   }
 }
